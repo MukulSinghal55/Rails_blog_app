64dba1ddd0f6ef180531cf80c21734155849ca1f
MukulSinghal55 <mukul.singhal@housing.com>
2020-09-01 11:21:22 +0530

    Created blog app

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,92 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+  # GET /blogs
+  # GET /blogs.json
+  def index
+    @blogs = Blog.all
+  end
+  
+  def create_comment
+    @comment=Comment.new(params.require(:comment).permit(:message,:blog_id,:user_id))
+    @comment.save
+    redirect_to @comment.blog  
+  end
+
+  def delete_comment
+    @comment = Comment.find(params[:id])
+    @blog=@comment.blog
+    @comment.destroy
+    redirect_to @blog
+  end
+
+  # GET /blogs/1
+  # GET /blogs/1.json
+  def show
+    @comments=Comment.where( blog_id: @blog.id).order('created_at DESC')
+
+    @new_comment=Comment.new
+    
+    puts @comments
+  end
+
+  # GET /blogs/new
+  def new
+    @blog = Blog.new
+  end
+
+  # GET /blogs/1/edit
+  def edit
+  end
+
+  # POST /blogs
+  # POST /blogs.json
+  def create
+    @blog = Blog.new(blog_params)
+
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /blogs/1
+  # PATCH/PUT /blogs/1.json
+  def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  def destroy
+    @blog.destroy
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Blog.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def blog_params
+      params.require(:blog).permit(:title,:content,:user_id)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :blogs
+  resources :users
+  post '/comment', to: 'blogs#create_comment'
+  post '/delete_comment/:id', to: 'blogs#delete_comment'
+  root to: 'blogs#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
