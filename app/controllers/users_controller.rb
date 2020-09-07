8c8208ec49e95a2ef74990253f6a6781d030b561
MukulSinghal55 <mukul.singhal@housing.com>
2020-09-07 13:29:41 +0530

    adding helper to check admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,9 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    if !is_accessible @user
+      return redirect_to root_path
+    end
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    def is_accessible(user)
+        return true if user_signed_in? && (current_user.admin||current_user.id==user.id)
+        false
+    end
 end
